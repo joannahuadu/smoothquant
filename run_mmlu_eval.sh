1be60de0193a803ea84d78ae599542e10efd4caa
huadu <joannahuadu@163.com>
2026-01-17 18:01:53 +0800

    bash scripts

--- run_mmlu_eval.sh
+++ run_mmlu_eval.sh
@@ -2,19 +2,22 @@
 
 export CUDA_VISIBLE_DEVICES=3
 
+MODEL_PATH="/gemini/code/checkpoints/models--meta-llama--Llama-3.1-8B/snapshots/d04e592bb4f6aa9cfee91e2e20afa771667e1d4b"
+ACT_SCALES_PATH="act_scales/llama-3.1-8B.pt"
+
 ALPHAS=(0.0 0.15 0.25 0.5 0.75 0.85 0.99)
 
 for alpha in "${ALPHAS[@]}"; do
     echo "Running with alpha=${alpha}"
     python benchmark_eval.py \
         --alpha ${alpha} \
-        --model_path /gemini/code/checkpoints/models--meta-llama--Llama-3.1-8B/snapshots/d04e592bb4f6aa9cfee91e2e20afa771667e1d4b \
-        --act_scales_path act_scales/llama-3.1-8B.pt \
+        --model_path ${MODEL_PATH} \
+        --act_scales_path ${ACT_SCALES_PATH} \
         --w_bits 8 \
         --a_bits 8 \
         --quantize \
         --tasks mmlu \
-        --num_fewshot 0  \
+        --num_fewshot 0 \
         --output_file /gemini/code/NMSparsity/smoothquant/results/models--meta-llama--Llama-3.1-8B_SQw8a8_alpha${alpha}_MMLU.json
 
 done
